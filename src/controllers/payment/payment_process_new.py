b1d7c94d2aa377aa94476a72b1ec0e508bcfe0aa
supriyan-adratesting <supriyan@mail.adraproductstudio.com>
2026-02-24 10:09:25 +0530

    Add baseline traces

--- src/controllers/payment/payment_process_new.py
+++ src/controllers/payment/payment_process_new.py
@@ -36,8 +36,6 @@ RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
 RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
 
 
-WH_SEC_KEY = 'whsec_uwbsFe2ICD0MPw9Rreyqy4lw81RzSShe'
-
 stripe.api_key = STRIPE_KEY
 
 try:
